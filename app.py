8803b8db13c917567f60f28e573359a4e510964f
izatul06 <01dgu24f1039@puo.mypolycc.edu.my>
2026-03-12 09:21:22 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projek Saya</title>
+</head>
+<body>
+    <h1>Selamat Datang!</h1>
+    <p>Ini adalah website yang saya buat guna GitHub Pages.</p>
+    </body>
+</html>
